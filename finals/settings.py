d63038b44d5838d271b1712f529df598c5fd6227
dvine21 <divineangelpialago671@gmail.com>
2025-06-14 20:10:13 +0800

    Trigger rebuild for static files

--- finals/settings.py
+++ finals/settings.py
@@ -104,8 +104,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [BASE_DIR / 'finalapp' / 'static']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
